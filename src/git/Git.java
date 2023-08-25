04d86700b5f619f11dfc797fc9286eb1afd8da0a
premnath <premnath9714@gmail.com>
2023-08-25 10:42:30 +0530

    Changes Committed

---
+++ src/git/Git.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is git folder");
+		System.out.println("This is git new folder");
+
+	}
+
+}
